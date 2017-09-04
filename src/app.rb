11d8f71a6a534208f1eece6d9d4de46d51e0b11c
Skeletonxf <skeletonxf@gmail.com>
2017-09-04 16:14:37 +0100

    Add first post

--- src/app.rb
+++ src/app.rb
@@ -2,7 +2,17 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class App < Sinatra::Base
+  nav = [
+      ["/building", "Building this site", "2017/09/04"]
+    ]
+
   get '/' do
+    @nav = nav
     erb :'index'
+  end
+
+  get '/building' do
+    @nav = nav
+    erb :'building'
   end
 end
