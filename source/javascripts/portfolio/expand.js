a80a96c027c23de392d95d7ffb63854caed8e7f2
Skeletonxf <skeletonxf@gmail.com>
2018-10-19 16:11:59 +0100

    Develop image -> text transformations

---
+++ source/javascripts/portfolio/expand.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+function hide(element) {
+  element.classList.remove('expanded')
+  element.classList.add('hidden')
+  element.setAttribute('aria-expanded', 'false')
+}
+
+function show(element) {
+  element.classList.add('expanded')
+  element.classList.remove('hidden')
+  element.setAttribute('aria-expanded', 'true')
+}
+
+// Hide all expanded divs
+// This way if JS is blocked then the divs are visible by default
+
+items = document.querySelectorAll('.expanded')
+console.log(items)
+items.forEach(i => i.classList.add('js-works'))
+items.forEach(hide)
+
+document.addEventListener('focusin', (event) => {
+  let item = event.target
+  let hidden = item.querySelectorAll('.hidden')
+  hidden.forEach(show)
+})
+
+document.addEventListener('focusout', (event) => {
+  let item = event.target
+  let hidden = item.querySelectorAll('.expanded')
+  hidden.forEach(hide)
+})
+
+})
